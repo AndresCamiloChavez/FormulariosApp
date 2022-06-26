5be202229799d60e69bee8ee150ce4e0485dd176
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-06-25 22:19:14 -0500

    Fomularios Dinamicos con template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.css']
+  styleUrls: ['./dinamicos.component.css'],
 })
 export class DinamicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Andres',
+    favoritos: [
+      {id: 1,nombre: 'Manzana'},
+      {id: 2,nombre: 'Peras'},
+    ],
+  };
+  nombreNuevoFavorito = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    console.log('Formulario posteado');
   }
 
+  validacionNombre() {
+    return (
+      this.miFormulario?.controls['nombre']?.value.length <= 0 &&
+      this.miFormulario?.controls['nombre']?.touched
+    );
+  }
+  deleteFavorito(favorito: Favorito){
+    this.persona.favoritos = this.persona.favoritos.filter(element => element.id != favorito.id);
+  }
+  deleteByIndexFavorito(index: any){
+    this.persona.favoritos.splice(index, 1);
+  }
+  agregarFavorito(){
+    const mfavorito = {
+      id: this.persona.favoritos.length,
+      nombre: this.nombreNuevoFavorito
+    }
+    this.persona.favoritos.push({...mfavorito});
+    this.nombreNuevoFavorito = '';
+  }
 }

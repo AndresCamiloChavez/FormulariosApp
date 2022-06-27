c139a7c5c99602c33c2101da74ce2641371fb096
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-06-26 20:23:35 -0500

    Uso de formularios condicionales

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre'      : new FormControl('Andrés'),
+  //   'precio'      : new FormControl(1500),
+  //   'existencias' : new FormControl(30),
+  // });
+  constructor(private formBuilder: FormBuilder) { }
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    'nombre': [, [Validators.required, Validators.minLength(3)]],
+    'precio': [, [Validators.required, Validators.min(0)]],
+    'existencias': [,[Validators.required, Validators.min(10)]],
+  });
+
 
   ngOnInit(): void {
+    this.miFormulario.setValue({
+      nombre: 'Andres',
+      precio: 0,
+      existencias: 0
+    });
+
+    this.miFormulario.get('nombre')?.setValue('Camilo');
+  }
+
+  campoEsValido(campo: any){
+    return this.miFormulario.controls[campo]?.errors && this.miFormulario.controls[campo].touched
+  }
+  guardar(){
+    if(!this.miFormulario.invalid){
+      return;
+    }
+    // this.miFormulario.markAllAsTouched();
+    this.miFormulario.reset();
+    console.log('miFormulario', this.miFormulario.value);
+    
   }
 
 }

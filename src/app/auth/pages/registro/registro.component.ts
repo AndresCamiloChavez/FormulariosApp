e1b867bee2393e1cefc7ae535f56ec8d30faff46
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-06-29 21:31:40 -0500

    Manejo de errores personalizados en formularios

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
+  nombreApellidoPattern,
+  noPuedeSerStrider,
+} from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from '../../services/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -13,20 +13,41 @@ import { ValidatorService } from '../../services/validator.service';
   styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
-  constructor(private fb: FormBuilder, private validatorService: ValidatorService) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
+  ) {}
 
-  miFormulario: FormGroup = this.fb.group({
-    nombre: [
-      '',
-      [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)],
-    ],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
-    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
-  },{
-    validators: [ this.validatorService.camposIguales('password', 'password2')]
-  });
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.nombreApellidoPattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],[this.emailValidator]
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorService.noPuedeSerStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorService.camposIguales('password', 'password2'),
+      ],
+    }
+  );
 
   ngOnInit(): void {
     this.miFormulario.reset({
@@ -44,5 +65,17 @@ export class RegistroComponent implements OnInit {
   }
   guardar() {
     this.miFormulario.markAllAsTouched();
+  }
+  emailRequired(){  
+    return (
+      this.miFormulario.get('email')?.hasError('required') && this.miFormulario.get('email')?.touched );
+  }
+  emailPattern(){  
+    return (
+      this.miFormulario.get('email')?.hasError('pattern') && this.miFormulario.get('email')?.touched );
+  }
+  emailTomato(){  
+    return (
+      this.miFormulario.get('email')?.hasError('emailExiste') && this.miFormulario.get('email')?.touched );
   }
 }
